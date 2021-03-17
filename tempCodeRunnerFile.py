0d82ced2f845bce06ee89abb776480bb3d94e8f8
Geo Jolly <69108486+kingjuno@users.noreply.github.com>
2021-03-17 20:31:03 +0530

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-for i in mars_weather['sol_keys']:
-        sol_data.append(mars_weather[str(i)]['First_UTC'][5:10])
-    month = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
-    for i in sol_data:
-        sol_final.append(str(month[int(i[:2])-1])+'. '+i[3:])
